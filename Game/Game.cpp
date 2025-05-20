453d7d33ac5076a4e1301f7a59d7a721ea5bd059
imase <imase0730@gmail.com>
2025-05-20 09:22:07 +0900

    プロジェクト ファイルを追加します。

---
+++ Game/Game.cpp
@@ -0,0 +1,108 @@
+﻿/**
+ * @file   Game.cpp
+ *
+ * @brief  ゲーム全体に関するソースファイル
+ *
+ * @author 制作者名
+ *
+ * @date   日付
+ */
+
+// ヘッダファイルの読み込み ===================================================
+#include "Game.h"
+
+#include "Game/Screen.h"
+
+
+
+
+// メンバ関数の定義 ===========================================================
+/**
+ * @brief デフォルトコンストラクタ
+ *
+ * @param なし
+ */
+Game::Game()
+	: m_key{ 0 }
+	, m_oldKey{ 0 }
+{
+	// 乱数の初期値を設定
+	SRand(static_cast<int>(time(nullptr)));
+}
+
+
+
+/**
+ * @brief デストラクタ
+ */
+Game::~Game()
+{
+
+}
+
+
+
+/**
+ * @brief 初期化処理
+ *
+ * @param なし
+ *
+ * @return なし
+ */
+void Game::Initialize()
+{
+	// ゲームの初期化
+
+}
+
+
+
+/**
+ * @brief 更新処理
+ *
+ * @param なし
+ *
+ * @return なし
+ */
+void Game::Update(float elapsedTime)
+{
+	// キー入力の取得
+	m_oldKey = m_key;
+	m_key    = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+
+
+	// ゲームの更新
+
+}
+
+
+
+/**
+ * @brief 描画処理
+ *
+ * @param[in] なし
+ *
+ * @return なし
+ */
+void Game::Render()
+{
+	// ゲームの描画
+
+
+
+}
+
+
+
+/**
+ * @brief 終了処理
+ *
+ * @param なし
+ *
+ * @return なし
+ */
+void Game::Finalize()
+{
+	// ゲームの終了処理
+
+}
